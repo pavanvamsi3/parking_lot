c81965723d0659a77fa85d29e202ae5173c87c67
Vamsi <pavanvamsi3@gmail.com>
2018-09-03 16:29:24 +0530

    vehicle class

---
+++ vehicle.py
@@ -0,0 +1,19 @@
+class Vehicle():
+
+    def __init__(self, vehicle_size, license_plate, spot_size):
+        self.vehicle_size = vehicle_size
+        self.license_plate = license_plate
+        self.spot_size = spot_size
+        self.spots_taken = []
+
+    def clear_spots(self):
+        for spot in self.spots_taken:
+            spot.remove_vehicle(self)
+        self.spots_taken = []
+
+    def take_spot(self, spot):
+        self.spots_taken.append(spot)
+
+    @abstractmethod
+    def can_fit_in_spot(self, spot):
+        pass
